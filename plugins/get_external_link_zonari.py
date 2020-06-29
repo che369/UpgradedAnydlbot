68b3e7dedcdedaffebe6ee90b816299a45ec9687
che369 <63643345+che369@users.noreply.github.com>
2020-06-29 12:38:33 +0530

    Update get_external_link_zonari.py

--- plugins/get_external_link_zonari.py
+++ plugins/get_external_link_zonari.py
@@ -103,7 +103,7 @@ def get_link(bot, update):
     filesize = os.path.getsize(after_download_file_name)
     filename = os.path.basename(after_download_file_name)
     
-    url = "https://upload369test.herokuapp.com/{}".format(filename)
+    url = "https://transfer.zonari.us/{}".format(filename)
     max_days = "2"
     command_to_exec = [
         "curl", "-g",
